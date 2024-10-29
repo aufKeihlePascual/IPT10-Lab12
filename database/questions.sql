e113780281188f604a8a15496aea47e888208a45
aufKeihlePascual <pascual.keihlediannegyraser@auf.edu.ph>
2024-10-29 18:13:30 +0800

    Added questions.sql

--- database/questions.sql
+++ database/questions.sql
@@ -1,3 +1,7 @@
 CREATE TABLE questions (
-    
-)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    item_number INT NOT NULL,
+    question TEXT NOT NULL,
+    choices JSON NOT NULL,
+    correct_answer CHAR(1) NOT NULL
+);
